825c986a1bd0a7a38677a344fa4cc57d880c9a66
Toli Carter <DragonMaster613@msn.com>
2015-04-17 18:39:31 -0700

    starting pythong maya book again

---
+++ envSetup.bat
@@ -0,0 +1,3 @@
+set PATH=%PATH%;c:\Program Files\Autodesk\Maya2014\bin
+set PYTHONPATH=%PYTHONPATH%;c:\projects\mayapybook\pylib
+start mayapy
---
+++ pylib/minspect.py
@@ -0,0 +1,6 @@
+import sys
+
+def syspath():
+	print 'sys.path:'
+	for p in sys.path:
+		print ' ' + p
